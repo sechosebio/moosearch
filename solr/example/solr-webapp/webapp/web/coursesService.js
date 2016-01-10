ebc4a8947a6d4a80fca4c3d52465380972326fa5
Jose Sebio <jalopezsebio@gmail.com>
2016-01-10 17:34:39 +0100

    Añadido buscador

--- solr/example/solr-webapp/webapp/web/coursesService.js
+++ solr/example/solr-webapp/webapp/web/coursesService.js
@@ -1,9 +1,16 @@
 app.factory('coursesService', function ($http, $rootScope) {
 
     var url = "http://localhost:8983/solr/collection1/select?q=*%3A*&rows=100000&wt=json&callback=JSON_CALLBACK"
+    var url_base = "http://localhost:8983/solr/collection1/select?q="
+    var url_end = "&rows=100000&wt=json&callback=JSON_CALLBACK"
     return {
         all: function() {
             return $http.get(url).then(function(result){
+                return result.data;
+            });
+        },
+        query: function(q){
+            return $http.get(url_base+q+url_end).then(function(result){
                 return result.data;
             });
         }
--- solr/example/solr-webapp/webapp/web/homeController.js
+++ solr/example/solr-webapp/webapp/web/homeController.js
@@ -1,4 +1,4 @@
-app.controller('homeController',function($scope,$uibModal,$log,coursesService){
+app.controller('homeController',function($scope,$uibModal,$log,$http,coursesService){
 
 
     /*$scope.courses = [
@@ -38,10 +38,10 @@ app.controller('homeController',function($scope,$uibModal,$log,coursesService){
         photoUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera.s3.amazonaws.com/topics/gamification/large-icon.png"
     }];*/
 
-    coursesService.all().then(function(data){
+    /*coursesService.all().then(function(data){
 
         $scope.courses = data.response.docs;
-    })
+    })*/
 
     $scope.open = function (course) {
 
@@ -65,6 +65,37 @@ app.controller('homeController',function($scope,$uibModal,$log,coursesService){
   $scope.toggleAnimation = function () {
     $scope.animationsEnabled = !$scope.animationsEnabled;
   };
+
+  $scope.selectedCourse = undefined;
+  var url_base = "http://localhost:8983/solr/collection1/select?q="
+  var url_end = "&rows=100000&wt=json"
+  $scope.getCourses = function(val) {
+      var url = url_base+"nombre%3A"+val+"*+OR+descripcion%3A"+val+url_end;
+      return $http.get(url).then(function(response){
+        return response.data.response.docs.map(function(item){
+
+          return item;
+        });
+      });
+    };
+
+   $scope.search = function(){
+       if($scope.checkBoxCoursera && $scope.checkBoxUdacity){
+           console.log("ambos")
+           var url = "http://localhost:8983/solr/collection1/select?q=pagina%3A+(udacity+%7C%7C+coursera)&rows=100000&wt=json&indent=true"
+       }else if($scope.checkBoxCoursera){
+           var url = "http://localhost:8983/solr/collection1/select?q=pagina%3A+coursera%0A&rows=100000&wt=json&indent=true"
+           console.log("coursera")
+       }else if($scope.checkBoxUdacity){
+           var url = "http://localhost:8983/solr/collection1/select?q=pagina%3A+udacity%0A&rows=100000&wt=json&indent=true"
+       }else{
+          var url ="http://localhost:8983/solr/collection1/select?q=*%3A*%0A&rows=100000&wt=json&indent=true"
+       }
+       return $http.get(url).then(function(result){
+           $scope.courses = result.data.response.docs;
+       });
+
+   };
 
 
 });
